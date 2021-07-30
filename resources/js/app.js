d5ef1765554c7744a4a4c6a48f9b8645b5c74edb
Hanna Herasimava <annlisandias@gmail.com>
2021-07-30 18:17:40 +0200

    Add Vue.js Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import Vue from 'vue';
+import SubmitTicketFormComponent from './components/SubmitTicketFormComponent.vue';
+
 require('./bootstrap');
+
+window.Vue = Vue;
+
+const app = new Vue({
+    el: '#app',
+    render: f => f(SubmitTicketFormComponent)
+});
